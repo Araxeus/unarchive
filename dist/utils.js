0b05e4527a47931f9eecbb1249c77db61e266dd2
Araxeus <78568641+Araxeus@users.noreply.github.com>
2023-03-24 22:06:34 +0300

    remove dist from git

--- dist/utils.js
+++
@@ -1,5 +0,0 @@
-import path from 'node:path';
-export function removeExtension(input) {
-    const res = input.slice(0, input.lastIndexOf(path.extname(input)));
-    return res.endsWith('.tar') ? res.slice(0, -4) : res;
-}
